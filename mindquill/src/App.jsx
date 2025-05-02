b8d7cd2edd97423f9a5101edfb6c56be0f97f64e
TheSimmer101 <rachelbriskman@gmail.com>
2025-05-02 15:44:56 -0400

    embed ai

--- mindquill/src/App.jsx
+++ mindquill/src/App.jsx
@@ -1,3 +1,40 @@
+
+// import React, { useState } from "react";
+// import axios from "axios";
+
+// const API_KEY = "AIzaSyDuYyzAp6Kmx0ImIzv7ZVYHvkaRgdGK56Q";
+// const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyDuYyzAp6Kmx0ImIzv7ZVYHvkaRgdGK56Q`;
+// //`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.1-pro:generateContent?key=AIzaSyDuYyzAp6Kmx0ImIzv7ZVYHvkaRgdGK56Q`;
+// function App() {
+//   const [responseText, setResponseText] = useState("");
+
+//   const fetchAIResponse = async () => {
+//     try {
+//       const response = await axios.post(API_URL, {
+//         contents: [{ parts: [{ text: "In 50 words max, write a poem about flowers" }] }],
+//       });
+
+//       setResponseText(response.data?.candidates?.[0]?.content?.parts?.[0]?.text  ?? "No response");
+//     } catch (error) {
+//       console.error("Error:", error);
+//       setResponseText("Error fetching AI response.");
+//     }
+//   };
+//   // console.log("API Response:", response.data);
+
+// console.log({responseText});
+
+//   return (
+//     <div>
+//       <h1>AI Response</h1>
+//       <button onClick={fetchAIResponse}>Ask AI</button>
+//       {/* <p>The response is: {responseText}</p> */}
+//       <p>{responseText}</p>
+//     </div>
+//   );
+// }
+
+// export default App;
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
